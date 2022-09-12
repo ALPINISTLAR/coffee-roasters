8711a911ee9dc403099d75e45b840b975b38f144
ALPINISTLAR <niveam424@gmail.com>
2022-09-12 11:23:23 +0200

    add input classes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const elTitleButton2 = document.querySelector('.item-header-2');
 const elTitleButton3 = document.querySelector('.item-header-3');
 const elTitleButton4 = document.querySelector('.item-header-4');
 const elTitleButton5 = document.querySelector('.item-header-5');
+const elRadioButton = document.querySelector('.input-check-box');
 
 elTitleButton.addEventListener('click', function () {
   document.querySelector('.item-body').classList.toggle('item-body-open')
@@ -27,4 +28,8 @@ elTitleButton4.addEventListener('click', function () {
 elTitleButton5.addEventListener('click', function () {
   document.querySelector('.item-body-5').classList.toggle('item-body-open')
   document.querySelector('.item-header-title-icon-5').classList.toggle('item-header-title-icon-open')
+});
+
+elRadioButton.addEventListener('checked', function () {
+  document.querySelector('.input-check-box-heading').classList.add('input-check-box-heading-act')
 });
