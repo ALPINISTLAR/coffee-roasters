4b3a613610d2182af5e1957461fb9f78777b50a4
ALPINISTLAR <niveam424@gmail.com>
2023-07-04 09:39:26 +0200

    fix item-header toggler and add 'change form options when input checked' function

--- js/main.js
+++ js/main.js
@@ -1,35 +1,62 @@
-const elTitleButton = document.querySelector('.item-header');
-const elTitleButton2 = document.querySelector('.item-header-2');
-const elTitleButton3 = document.querySelector('.item-header-3');
-const elTitleButton4 = document.querySelector('.item-header-4');
-const elTitleButton5 = document.querySelector('.item-header-5');
-const elRadioButton = document.querySelector('.input-check-box');
+const elsItemHeaderToggler = document.querySelectorAll('.item-header-toggler');
 
-elTitleButton.addEventListener('click', function () {
-  document.querySelector('.item-body').classList.toggle('item-body-open')
-  document.querySelector('.item-header-title-icon').classList.toggle('item-header-title-icon-open')
+elsItemHeaderToggler.forEach(function (elItemHeaderToggler) {
+  elItemHeaderToggler.addEventListener('click', function () {
+    elItemHeaderToggler.closest('.questionnaire-list-item').classList.toggle('questionnaire-list-item--open');
+    elItemHeaderToggler.children[0].classList.toggle('item-header-toggler-icon-open')
+  });
 });
 
-elTitleButton2.addEventListener('click', function () {
-  document.querySelector('.item-body-2').classList.toggle('item-body-open')
-  document.querySelector('.item-header-title-icon-2').classList.toggle('item-header-title-icon-open')
-});
 
-elTitleButton3.addEventListener('click', function () {
-  document.querySelector('.item-body-3').classList.toggle('item-body-open')
-  document.querySelector('.item-header-title-icon-3').classList.toggle('item-header-title-icon-open')
-});
 
-elTitleButton4.addEventListener('click', function () {
-  document.querySelector('.item-body-4').classList.toggle('item-body-open')
-  document.querySelector('.item-header-title-icon-4').classList.toggle('item-header-title-icon-open')
-});
 
-elTitleButton5.addEventListener('click', function () {
-  document.querySelector('.item-body-5').classList.toggle('item-body-open')
-  document.querySelector('.item-header-title-icon-5').classList.toggle('item-header-title-icon-open')
-});
+document.getElementsByName('drink').forEach(function (elInputRadioButton) {
+  elInputRadioButton.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      document.getElementById('preferences').textContent = event.target.value;
+    } else {
+      document.getElementById('preferences').textContent = '_____'
+    }
+  })
+})
 
-elRadioButton.addEventListener('checked', function () {
-  document.querySelector('.input-check-box-heading').classList.add('input-check-box-heading-act')
-});
+
+document.getElementsByName('type').forEach(function (elInputRadioButton) {
+  elInputRadioButton.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      document.getElementById('type').textContent = event.target.value;
+    } else {
+      document.getElementById('type').textContent = '_____'
+    }
+  })
+})
+
+document.getElementsByName('like').forEach(function (elInputRadioButton) {
+  elInputRadioButton.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      document.getElementById('quantity').textContent = event.target.value;
+    } else {
+      document.getElementById('quantity').textContent = '_____'
+    }
+  })
+})
+
+document.getElementsByName('grind').forEach(function (elInputRadioButton) {
+  elInputRadioButton.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      document.getElementById('grind').textContent = event.target.value;
+    } else {
+      document.getElementById('grind').textContent = '_____'
+    }
+  })
+})
+
+document.getElementsByName('deliver').forEach(function (elInputRadioButton) {
+  elInputRadioButton.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      document.getElementById('deliveries').textContent = event.target.value;
+    } else {
+      document.getElementById('deliveries').textContent = '_____'
+    }
+  })
+})
